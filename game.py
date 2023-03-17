c8d4def0680b1c998ba7dee9ff51f34a4557e7cf
MateoCarusotti <mateocarusotti@gmail.com>
2023-03-17 13:53:45 -0300

    Agrego la respuesta si se divide por Cero

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ from random import choice, randrange
 from datetime import datetime
 
 
-operators = ["+", "-","*","/"]
+operators = ["/"]
 times = 5
 init_time = datetime.now()
 print(f"¡Veremos cuanto tardas en responder estas {times} operaciones!")
@@ -12,23 +12,26 @@ for i in range(0, times):
     number_1 = randrange(10)
     number_2 = randrange(10)
     operator = choice(operators)
-    print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?") 
-    match operator:
-        case "+": 
-            res = number_1 + number_2
-        case "-":
-            res = number_1 - number_2
-        case "*":
-            res = number_1 * number_2
-        case "/":
-            res = number_1 / number_2        
-    result = input("resultado: ")
-    if  int (result) == int (res):
-        print("Correcto")
-        correct += 1
+    if number_2 != 0:    
+        print(f"{i+1}- ¿Cuánto es {number_1} {operator} {number_2}?") 
+        match operator:
+            case "+": 
+                res = number_1 + number_2
+            case "-":
+                res = number_1 - number_2
+            case "*":
+                res = number_1 * number_2
+            case "/":
+                res = number_1 / number_2
+        result = input("resultado: ")
+        if  int (result) == int (res):
+            print("Correcto")
+            correct += 1
+        else:
+            print("Incorrecto")
+            incorrect += 1               
     else:
-        print("Incorrecto")
-        incorrect += 1       
+        print("No se ejecuto la operacion ya que habia division por Cero")
 end_time = datetime.now()
 total_time = end_time - init_time
 print(f"\n Tardaste {total_time.seconds} segundos. Con {correct} aciertos y {incorrect} desaciertos")  
